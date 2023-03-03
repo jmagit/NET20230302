296e202eaaf80a73a888d11b07bc741f9e6dfe3d
Javier <jmartin@grupoloyal.com>
2023-03-03 13:09:44 +0100

    Demos

--- Demos/Documentada.cs
+++ Demos/Documentada.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Math;
 
 namespace Demos {
     /// <summary>
@@ -24,18 +25,49 @@ namespace Demos {
         /// <param name="a">Operando 1</param>
         /// <param name="b">Operando 2</param>
         /// <returns>Suma de los valores</returns>
-        public decimal suma(decimal a, decimal b) {
+        public decimal suma(decimal a, decimal b = 0, decimal c = 0) {
             #region Métodos
-            return a + b;
+            return Abs(a + b + c);
             #endregion
         }
+        public decimal avg(decimal a, params decimal[] lst) {
+            return Abs(a);
+        }
         public string nombre() {
+            avg(1, 2, 3, 4);
+            suma(4, c: 4);
             return nameof(suma);
         }
+
         #endregion
     }
+}
 
-    public class SubDocumento : Documentada {
+namespace Demos.Curso {
+    public class Documentada {
+            #region Atributos
+
+            #endregion
+            #region Métodos
+
+            /// <summary>
+            ///     Suma dos valores
+            /// </summary>
+            /// <param name="a">Operando 1</param>
+            /// <param name="b">Operando 2</param>
+            /// <returns>Suma de los valores</returns>
+            public decimal suma(decimal a, decimal b) {
+                #region Métodos
+                return a + b;
+                #endregion
+            }
+            public string nombre() {
+                return nameof(suma);
+            }
+            #endregion
+        }
+
+        public class SubDocumento : Demos.Documentada {
 
     }
 }
